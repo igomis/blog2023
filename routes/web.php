d73c3c577aa53847bb2eefb36afd7967207f2733
igomis <igomis@cipfpbatoi.es>
2023-12-12 12:10:30 +0100

    732

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('posts',PostController::class)->except(['index','show']);
+    Route::resource('posts',PostController::class)->only(['create','store']);
 });
-
 require __DIR__.'/auth.php';
 
+Route::resource('/posts', 'PostController')->only('edit','update','destroy')->middleware('isAuthor');
 Route::resource('posts',PostController::class)->only(['index','show']);
